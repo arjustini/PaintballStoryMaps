2e6fe9938cf558f86850ce701d9b9230c23120bb
Alex <arjustini96@gmail.com>
2020-03-30 15:26:39 -0500

    pylons added

---
+++ PBS/NewGrass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="NewGrass" tilewidth="64" tileheight="64" tilecount="16" columns="8">
+ <image source="../Maps/NewGrass.png" width="512" height="128"/>
+</tileset>
